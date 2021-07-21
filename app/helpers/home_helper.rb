ef99836ea89bb8d86d7d920af18ed8dcbdf584fa
Filipe Mendes Webber <filipewebber@filipewebber.dev>
2021-07-21 19:40:05 -0300

    Create users.username column, create home/index.html and create function to create proper youtube links

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,7 @@
 module HomeHelper
+    def embed_yt(link)
+        url_split = link.split('/watch?v=')
+        new_address = url_split[0] + "/embed/" + url_split[1]
+        return new_address.split('&')[0]
+    end
 end
