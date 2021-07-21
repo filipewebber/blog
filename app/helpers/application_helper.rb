d8c5b7bd0f3e57c32416491bc0d1677326fe7063
Filipe Mendes Webber <filipewebber@filipewebber.dev>
2021-07-21 12:00:47 -0300

    Fixed navbar button highlighting for posts CRUD pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,12 @@ module ApplicationHelper
             "btn current"
         elsif request.path == '/users' && i == 1
             "btn current"
+        elsif request.path == '/posts' && i == 2
+            "btn current"
+        elsif request.path == '/posts/new' && i == 2
+            "btn current"
+        elsif request.path.include?('posts') && request.path.include?('edit') && i == 2
+            "btn current"
         else
             "btn idle"
         end
