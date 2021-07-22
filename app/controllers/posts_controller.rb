a60eb512f31dc0bfed6f913692df895cb57878e6
Filipe Mendes Webber <filipewebber@filipewebber.dev>
2021-07-22 13:27:59 -0300

    Update posts controller to allow the title parameter to be edited.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -71,6 +71,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:user_id, :url, :description)
+      params.require(:post).permit(:user_id, :title, :url, :description)
     end
 end
